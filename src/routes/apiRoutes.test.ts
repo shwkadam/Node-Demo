90771c3a0b9925156ea82613ccf4f87d4a856aaa
Shweta Kadam <shweta.kadam@hitachids.com>
2025-04-07 17:38:32 +0530

    added test cases

--- src/routes/apiRoutes.test.ts
+++ src/routes/apiRoutes.test.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { fetchUsers, fetchPosts } from './apiRoutes'; // Adjust the path based on your project structure
+import { fetchUsers, fetchPosts } from './apiRoutes'; 
 
 jest.mock('axios');
 
